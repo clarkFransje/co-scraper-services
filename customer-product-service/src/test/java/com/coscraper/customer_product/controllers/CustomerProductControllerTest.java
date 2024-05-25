1b2663e17f92fc7d4e9d097fda5f528068d3aafd
Arnold Francisca <47351180+clarkFransje@users.noreply.github.com>
2024-05-26 01:31:41 +0200

    Updated tests

--- customer-product-service/src/test/java/com/coscraper/customer_product/controllers/CustomerProductControllerTest.java
+++ customer-product-service/src/test/java/com/coscraper/customer_product/controllers/CustomerProductControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,7 +30,7 @@ public class CustomerProductControllerTest {
 
     @Test
     public void testAddProductToCustomer() throws Exception {
-        CustomerProductAddRequest request = new CustomerProductAddRequest(1, 1);
+        CustomerProductAddRequest request = new CustomerProductAddRequest(UUID.fromString("0851d075-e483-477f-a0fc-c5f8779efb02"), UUID.fromString("297b5734-2c51-4f6f-9775-96ce3f5c9eab");
 
         mockMvc.perform(post("/api/v1")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -38,7 +40,7 @@ public class CustomerProductControllerTest {
 
     @Test
     public void testRemoveProductFromCustomer() throws Exception {
-        CustomerProductDeleteRequest request = new CustomerProductDeleteRequest(1);
+        CustomerProductDeleteRequest request = new CustomerProductDeleteRequest(UUID.fromString("0851d075-e483-477f-a0fc-c5f8779efb02"));
 
         mockMvc.perform(delete("/api/v1")
                         .contentType(MediaType.APPLICATION_JSON)
