e8408d83ea388e3e9e5d0ebc04d44c46b48093cf
Arnold Francisca <47351180+clarkFransje@users.noreply.github.com>
2024-05-26 02:00:09 +0200

    Fixed test for customer-service

--- customer-service/src/test/java/com/coscraper/customer/controllers/CustomerControllerTest.java
+++ customer-service/src/test/java/com/coscraper/customer/controllers/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package com.coscraper.customer.controllers;
 
 import com.coscraper.customer.controller.CustomerController;
+import com.coscraper.customer.models.Customer;
 import com.coscraper.customer.models.CustomerAddRequest;
 import com.coscraper.customer.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,13 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.ArgumentMatchers.any;
 
 @WebMvcTest(CustomerController.class)
@@ -32,16 +35,25 @@ public class CustomerControllerTest {
 
     @Test
     public void testRegisterCustomer() throws Exception {
+        // Create a CustomerAddRequest object for testing
         CustomerAddRequest request = new CustomerAddRequest("John", "Doe", "john.doe@example.com");
 
+        // Mocking the behavior of registerCustomer method to return Optional of Customer
+        when(customerService.registerCustomer(any(CustomerAddRequest.class)))
+                .thenReturn(Optional.of(new Customer()));
+
+        // Perform a POST request to the specified endpoint with the request content
         mockMvc.perform(post("/api/v1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
+                // Verify that the status code is 200 (OK)
                 .andExpect(status().isOk());
 
-        // Mocking the behavior of registerCustomer method
-        doNothing().when(customerService).registerCustomer(any(CustomerAddRequest.class));
+        // Verify that the registerCustomer method is invoked with the correct argument
+        verify(customerService, times(1)).registerCustomer(eq(request));
     }
+
+
 
     @Test
     public void testDeleteCustomer() throws Exception {
